ded3a865717bd607fcf3e6de0fb214f60458ec5f
siddheshmore0024@gmail.com <siddheshmore0024@gmail.com>
2024-10-09 09:31:29 +0530

    homepage dummy data

---
+++ lib/homescreen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Dummy
+  List<Map<String, dynamic>> _chatHistory = [
+    {
+      'id': 1,
+      'last_message': 'Hello , this is Chat 1 ',
+    },
+    {
+      'id': 2,
+      'last_message': 'Hello , this is Chat 2',
+    },
+    {
+      'id': 3,
+      'last_message': 'Hello , this is Chat 3',
+    },
+    {
+      'id': 4,
+      'last_message': 'Hello , this is Chat 4',
+    },
+    {
+      'id': 5,
+      'last_message': 'Hello , this is Chat 5',
+    },
+  ];
+
+
+
+  Widget _buildConversationTile(Map<String, dynamic> chat) {
+    return ListTile(
+      title: Text('Chat ${chat['id']}'),
+      subtitle: Text(chat['last_message'] ?? 'No messages'),
+      onTap: () {
+
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat History'),
+      ),
+      body: ListView.builder(
+        itemCount: _chatHistory.length,
+        itemBuilder: (context, index) {
+          return _buildConversationTile(_chatHistory[index]);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        tooltip: 'Start New Conversation',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
